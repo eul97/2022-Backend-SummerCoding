21c686b443b52a706fd91ad945206ae131e143c8
강지수 <37580216+kang-jisu@users.noreply.github.com>
2022-07-14 01:25:15 +0900

    feat: 이름을 입력받아 출력 (#5)

--- src/main/java/com/landvibe/homework/HomeworkMain.java
+++ src/main/java/com/landvibe/homework/HomeworkMain.java
@@ -1,7 +1,17 @@
 package com.landvibe.homework;
 
+import java.util.Scanner;
+
 public class HomeworkMain {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("이름을 입력해주세요.");
+        String name = scanner.nextLine();
+
+        System.out.println(name+" 님 안녕하세요!");
     }
 }
