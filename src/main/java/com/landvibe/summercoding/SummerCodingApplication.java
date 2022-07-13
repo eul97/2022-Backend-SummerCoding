2be20de925805d62ab86c37dc2cc788385629899
강지수 <37580216+kang-jisu@users.noreply.github.com>
2022-07-13 23:29:59 +0900

    build: 초기 세팅 (#4)

---
+++ src/main/java/com/landvibe/summercoding/SummerCodingApplication.java
@@ -0,0 +1,13 @@
+package com.landvibe.summercoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landvibe/summercoding/SummerCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landvibe.summercoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
