1784d330e0e59ff0380d5c241bc406cb0d42c853
RyongJoon <k95yong@naver.com>
2022-07-20 10:46:16 +0900

    사전 과제 피드백 반영_1
    - Account에서 withdraw, isPossibleToWithdraw 분리
    - for문 내부 iterator 네이밍 변경
    - 주문하기 기능 내부 반복문에서 depth 한단계 줄임
    - 주문번호나 메뉴 이름으로 주문 했을 때 메뉴와 같은건지 판별하는 함수를 Menu Class 내부 메소드로 만들었는데 이렇게 하는게 맞는지는 모르겠음..ㅎㅎ
    - List 이름 변경 menuList > menus
      * orderedList는 주문 목록 이라는 의미 이므로 그냥 뒀음
    - Menu 클래스 멤버 변수 이름 변경 orderNumber > menuNumber

--- src/main/java/com/landvibe/homework/HomeworkMain.java
+++ src/main/java/com/landvibe/homework/HomeworkMain.java
@@ -10,13 +10,13 @@ public class HomeworkMain {
         Scanner sc = new Scanner(System.in);
         String choice, myName;
         int initAccount;
-        List<Menu> menuList = new ArrayList<>();
+        List<Menu> menus = new ArrayList<>();
         List<Menu> orderedList = new ArrayList<>();
-        menuList.add(new Menu(1, "짜장면", 6000));
-        menuList.add(new Menu(2, "간짜장", 6500));
-        menuList.add(new Menu(3, "설원", 15000));
-        menuList.add(new Menu(4, "짬뽕", 7000));
-        menuList.add(new Menu(5, "짬뽕밥", 7500));
+        menus.add(new Menu(1, "짜장면", 6000));
+        menus.add(new Menu(2, "간짜장", 6500));
+        menus.add(new Menu(3, "설원", 15000));
+        menus.add(new Menu(4, "짬뽕", 7000));
+        menus.add(new Menu(5, "짬뽕밥", 7500));
 
         System.out.println("이름을 입력해 주세요");
         myName = sc.nextLine();
@@ -24,43 +24,46 @@ public class HomeworkMain {
         initAccount = Integer.parseInt(sc.nextLine());
         Account myAccount = new Account(myName, initAccount);
 
-
-        while(true){
+        while (true) {
             System.out.println(
                     "\n1. 메뉴판 보기\n" +
-                    "2. 주문 하기\n" +
-                    "3. 주문 내역 보기\n" +
-                    "4. 내 잔고 보기\n" +
-                    "5. 끝내기\n\n" +
-                    "무엇을 할까요?"
+                            "2. 주문 하기\n" +
+                            "3. 주문 내역 보기\n" +
+                            "4. 내 잔고 보기\n" +
+                            "5. 끝내기\n\n" +
+                            "무엇을 할까요?"
             );
 
             choice = sc.nextLine();
 
-            switch (choice){
-                case "1": case "메뉴판 보기":
-                    for(Menu i : menuList){
-                        System.out.println(i.getOrderNumber() + " " + i.getName() + " " + i.getPrice());
+            switch (choice) {
+                case "1":
+                case "메뉴판 보기":
+                    for (Menu i : menus) {
+                        System.out.println(i.getMenuNumber() + " " + i.getName() + " " + i.getPrice());
                     }
                     break;
 
-                case "2": case "주문하기":
+                case "2":
+                case "주문하기":
                     System.out.println("어떤 메뉴를 주문하시겠습니까? 주문번호 혹은 메뉴명을 적어주세요");
                     String c = sc.nextLine();
-                    for(Menu i : menuList){
-                        if(Integer.toString(i.getOrderNumber()).equals(c) || i.getName().equals(c)){
-                            if(myAccount.withdrawIfPossible(i.getPrice())){
-                                System.out.println("주문이 완료되었습니다.");
-                                orderedList.add(i);
-                                break;
-                            }else{
-                                System.out.println("잔액이 부족합니다.");
-                            }
+                    for (Menu menu : menus) {
+                        if (!menu.isSameMenu(c))
+                            continue;
+                        if (myAccount.isPossibleToWithdraw(menu.getPrice())) {
+                            myAccount.withdraw(menu.getPrice());
+                            orderedList.add(menu);
+                            System.out.println("주문이 완료되었습니다.");
+                        } else {
+                            System.out.println("잔액이 부족합니다.");
                         }
+                        break;
                     }
                     break;
-                case "3": case "주문 내역 보기":
-                    if(orderedList.isEmpty()){
+                case "3":
+                case "주문 내역 보기":
+                    if (orderedList.isEmpty()) {
                         System.out.println("주문 내역이 없습니다.");
                         break;
                     }
@@ -69,15 +72,17 @@ public class HomeworkMain {
                         System.out.println("메뉴 : " + i.getName() + " 가격 : " + i.getPrice() + "원");
                     }
                     break;
-                case "4": case "내 잔고 보기":
+                case "4":
+                case "내 잔고 보기":
                     myAccount.printAccount();
                     break;
-                case "5": case "끝내기":
+                case "5":
+                case "끝내기":
                     return;
-                default :
+                default:
                     break;
             }
         }
-
     }
+
 }
